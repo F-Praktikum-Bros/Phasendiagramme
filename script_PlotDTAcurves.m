6c6a838d61bbed9d9c6bb363f83ae98821a2f9d2
mp1995a <max@pffr.de>
2017-10-20 23:46:34 +0200

    Patch 4.0
    Auswertung (daten) fertig
    - jetzt nur noch Auswertung schreiben

--- script_PlotDTAcurves.m
+++ script_PlotDTAcurves.m
@@ -9,16 +9,66 @@ TbGdScO3_raw2=dlmread('Messdaten\ExpDat_Tb_0.5Gd_0.5ScO3-2200-2x-3(unsere)_tab.t
 Al2O3_raw=dlmread('Messdaten\ExpDat_Tb_0.5Gd_0.5ScO3-2200-2x-3(unsere)_tab.txt','\t', 'F1..F4030');
 
 % read data from file (RE-Skandate Klimm, GdScO3 & DyScO3; K...Klimm)
-size(dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt'
-tempsMeasK=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'A1..A4030');
-GdScO3_raw1=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'B1..B4030');
-GdScO3_raw2=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'C1..C4030');
-DyScO3_raw1=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'F1..F4030');
-DyScO3_raw2=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'G1..G4030');
-Al2O3_rawK1=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'D1..D4030');
-Al2O3_rawK2=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'E1..E4030');
+tempsMeasK=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'A1..A2477');
+GdScO3_raw1=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'B1..B2477');
+GdScO3_raw2=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'C1..C2477');
+DyScO3_raw1=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'F1..F2477');
+DyScO3_raw2=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'G1..G2477');
+Al2O3_rawK1=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'D1..D2477');
+Al2O3_rawK2=dlmread('Messdaten\ExpDat_GdScO3_DyScO3_Al2O3-Eich-6_tab.txt','\t', 'E1..E2477');
+
+% Offset Temperatur (berechnet aus Differenz der gemessenen
+% Schmelztemperatur T_S und dem theoret. Referenzwert für Al2O3
+tempKorr1=198;
+tempKorr2=125.5;
+
+% Temperaturkalibrierung beider Messreihen
+tempsNew=tempsMeas+tempKorr1;
+tempsOld=tempsMeasK+tempKorr2;
+
+% Für bessere Darstellung im Plot
+GdScO3_1=GdScO3_raw1+2.65;
+GdScO3_2=GdScO3_raw2+2.3;
+TbGdScO3_1=TbGdScO3_raw1-1.8;
+TbGdScO3_2=TbGdScO3_raw2-2.25;
+DyTbScO3_1=DyTbScO3_raw1-3.4;
+DyTbScO3_2=DyTbScO3_raw2-3.15;
+DyScO3_1=DyScO3_raw1-4;
+DyScO3_2=DyScO3_raw2-4.3;
+
+figure(1)
+%title('DTA vs. Temperatur')
+hold on
+plot(tempsOld,GdScO3_1,'Color',[1 0 0]); % Gd
+plot(tempsOld,GdScO3_2,'Color',[0.8 0.2 0]); % Gd
+plot(tempsNew,TbGdScO3_1,'Color',[0.3 1 0.3]);  % TbGd
+plot(tempsNew,TbGdScO3_2,'Color',[0.4 0.8 0.2]);  % TbGd
+plot(tempsNew,DyTbScO3_1,'Color',[0.5 0 0.5]);  % DyTb
+plot(tempsNew,DyTbScO3_2,'Color',[0.3 0.1 0.3]);  % DyTb
+plot(tempsOld,DyScO3_1,'Color',[0 0 1]); % Dy
+plot(tempsOld,DyScO3_2,'Color',[0 0.2 0.8]); % Dy
+%line([2141 2141],[-10 10], 'Color', [1 0 0]);
+hold off
+axis([2090 2225 -4.7 1.35])
+xticks(2090:20:2225)
+yticks([])
+xtickangle(0)
+xlabel('Temperatur (°C)');
+ylabel('DTA (\muV/mg)');
 
 
-
+% figure2=figure('Position', [350 350 600 500]);
+% subplot(2,1,1);
+% hold on
+% plot(tempsOld,GdScO3_1); % Gd
+% plot(tempsOld,GdScO3_2); % Gd
+% plot(tempsNew,TbGdScO3_1);  % TbGd
+% plot(tempsNew,TbGdScO3_2);  % TbGd
+% plot(tempsNew,DyTbScO3_1);  % DyTb
+% plot(tempsNew,DyTbScO3_2);  % DyTb
+% plot(tempsOld,DyScO3_1); % Dy
+% plot(tempsOld,DyScO3_2); % Dy
+% hold off
+% subplot(2,1,2);
 
 
